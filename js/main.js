ca46c30cfe258ea14771d17724f5c31655d0ae48
Valerie Mettler <valeriemettler@gmail.com>
2015-08-28 16:40:48 -0700

    display all movie names, years, and locations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var url = 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD';
 $.getJSON(url, function(data) {
-    console.log(data.data[10][8]);
-    console.log(data.data[10][9]);
-    console.log(data.data[10][10]);
+    for (var i = 0; i < data.data.length; i++) {
+        $('#x').append("<div>Movie Name: " + data.data[i][8]
+                       + "<ul><li>Movie Year: " + data.data[i][9]
+                       + "</li><li>Movie Location: " + data.data[i][10]
+                       + '</li></ul></div>');
+    }
 });
