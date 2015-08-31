99579829e4e57d9f7544170a5ff342bb73b9ef11
Valerie Mettler <valeriemettler@gmail.com>
2015-08-30 19:17:42 -0700

    add button that sorts movies by year

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-var url = 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD';
-$.getJSON(url, function(data) {
-    for (var i = 0; i < data.data.length; i++) {
-        $('#x').append('<div class="card"><div class="name">' + data.data[i][8]
-                       + '</div><div class="info"><div class="location"><img src="img/mappin2.png"></img>  '  + data.data[i][10]
-                       + '</div><div class="year">' + data.data[i][9]
+var movies;
+
+var display = function () {
+    $('#x').html("");
+    for (var i = 0; i < movies.data.length; i++) {
+        $('#x').append('<div class="card"><div class="name">' + movies.data[i][8]
+                       + '</div><div class="info"><div class="location"><img src="img/mappin2.png"></img>  '  + movies.data[i][10]
+                       + '</div><div class="year">' + movies.data[i][9]
                        + '</div></div></div>');
     }
+}
+
+var url = 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD';
+$.getJSON(url, function(result) {
+    movies = result;
+    display();
 });
+
+
+$('#sort-year').click(function(){
+    movies.data.sort(function (a,b) {
+        var j = parseInt(a[9]);
+        var k = parseInt(b[9]);
+        if (j < k) {return -1};
+        if (j > k) {return 1};
+        if (j === k) {return 0};
+    });
+    display();
+});
+
+
+
