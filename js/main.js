5834b195b594e0d1ecaa9000714623d34c217b1e
Valerie Mettler <valeriemettler@gmail.com>
2015-08-30 19:16:50 -0700

    style movie cards, add location pin img

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var url = 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD';
 $.getJSON(url, function(data) {
     for (var i = 0; i < data.data.length; i++) {
-        $('#x').append("<div>Movie Name: " + data.data[i][8]
-                       + "<ul><li>Movie Year: " + data.data[i][9]
-                       + "</li><li>Movie Location: " + data.data[i][10]
-                       + '</li></ul></div>');
+        $('#x').append('<div class="card"><div class="name">' + data.data[i][8]
+                       + '</div><div class="info"><div class="location"><img src="img/mappin2.png"></img>  '  + data.data[i][10]
+                       + '</div><div class="year">' + data.data[i][9]
+                       + '</div></div></div>');
     }
 });
