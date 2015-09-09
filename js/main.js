cf7964cc4a657e98f3efe2f192ac0e4a1c352e3c
Valerie Mettler <valeriemettler@gmail.com>
2015-09-08 22:09:54 -0700

    display google map of SF

--- js/main.js
+++ js/main.js
@@ -1,5 +1,94 @@
 var movies;
 
+// var map;
+// function initMap() {
+// var myLatLng = {lat: 37.764222, lng: -122.423369};
+// map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 37.7833, lng: -122.4167},
+//     zoom: 12
+//   });
+//    var marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+    //title: 'SF Movie Map'
+//   });
+// }
+
+/*
+var map;
+var markers = [];
+
+function initMap() {
+
+var myLatLng = {lat: 37.764222, lng: -122.423369};
+
+map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7833, lng: -122.4167},
+    zoom: 12
+  });
+
+//event listener calls addMarker when map is clicked:
+map.addListener('click', function(event)){
+    addMarker(event.latLng);
+}
+
+//adds marker to map and pushes to markers array:
+function addMarker(location){
+   var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    //title: 'SF Movie Map'
+  });
+ markers.push(marker);
+}
+
+//sets the map on all markers in the array??
+function setMapOnAll(map){
+    for(var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+//deletes all markers in the array
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+}*/
+
+//var map;
+function initMap() {
+var myLatLng = {lat: 37.764222, lng: -122.423369};
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7833, lng: -122.4167},
+    zoom: 12
+  });
+
+var geocoder = new google.maps.Geocoder();
+
+//change this to link to address of movie clicked
+document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+    var marker = null;
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
 //original display function with name, year, and location and location pin image:
 // var display = function () {
 //     $('#x').html("");
@@ -38,7 +127,7 @@ var display2 = function(m) {
             var n = m[name];
         }
         if (n[0] === null) {
-            console.log("null value");
+            console.log("movies with null location values");
         } else {
             x = x + '<div class="card"><div class="name">' + name + '</div><div class="info">  ';
             for (var j = 0; j < n.length; j++) {
@@ -63,6 +152,8 @@ var locations = function (a) {
     return output;
 };
 
+
+
 var url = 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD';
 $.getJSON(url, function(result) {
     //console.log(result);///take from result and put into temp
@@ -75,6 +166,8 @@ $.getJSON(url, function(result) {
     var y = display2(b);
     $("#x").html(y);
 });
+
+
 
 
 // $('#sort-year').click(function(){
